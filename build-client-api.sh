7d295029eb2e5dfd0064e063281597b4b9fd1e45
Казенин Владимир <kazeninvladimir@gmail.com>
2024-03-30 21:16:00 +0300

    update build

--- build-client-api.sh
+++ build-client-api.sh
@@ -3,12 +3,12 @@
 rm -rf ./client
 
 for file in $(find ./ -name '*.yaml'); do
-  if [[ $file == *"gen_configs"* ]]; then
+  if [[ $file == *"configs"* ]]; then
     continue
   fi
 
   echo Generating for $file
-  java -jar ./generator.jar generate -i $file -g typescript-angular -o ./client -c ./gen_configs/ts.yaml
+  java -jar ./generator.jar generate -i $file -g typescript-angular -o ./client -c ./configs/ts.yaml
 done
 
 cp -r ./client "${1}"
--- build-server-api.sh
+++ build-server-api.sh
@@ -3,12 +3,12 @@
 rm -rf ./server
 
 for file in $(find ./ -name '*.yaml'); do
-  if [[ $file == *"gen_configs"* ]]; then
+  if [[ $file == *"configs"* ]]; then
     continue
   fi
 
   echo Generating for $file
-  java -jar ./generator.jar generate -i $file -g spring -o ./server -c ./gen_configs/java.yaml
+  java -jar ./generator.jar generate -i $file -g spring -o ./server -c ./configs/java.yaml
 done
 
 echo Building java
